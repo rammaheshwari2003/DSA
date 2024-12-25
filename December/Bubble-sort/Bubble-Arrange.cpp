df0867e822fb839ada3cf0e3757637da27aa5385
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-12-25 17:53:05 +0530

    Bubble-Arrange

--- December/Bubble-sort/Bubble-Arrange.cpp
+++ December/Bubble-sort/Bubble-Arrange.cpp
@@ -23,12 +23,18 @@ void bubble(int arr[],int s){
 }
 
 int main(){
-int arr[]={1,2,3,5,4};
+//int arr[]={1,2,3,5,4};
+int arr[]={5,4,3,2,1};
+
 int s=sizeof(arr)/sizeof(arr[0]);
 for(int i=0; i<s; i++){
-	
+	cout<<arr[i]<<"\t";
 }
-
+bubble(arr,s);
+cout<<"\nAfter sort array : \n";
+for(int i=0; i<s; i++){
+	cout<<arr[i]<<"\t";
+}
 
 
 return 0;
