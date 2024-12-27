d586922ca9319e226049a581793f3e5853ee5e63
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-12-27 21:46:27 +0530

    Basic

---
+++ December/Insertion Sort/1st- Basic.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void insrt(int arr[], int s){
+	for(int i=1; i<s; i++){
+		int chotu=i;
+		while(chotu>0 && arr[chotu]<arr[chotu-1]){
+			int tmp;
+			tmp=arr[chotu-1];
+			arr[chotu-1]=arr[chotu];
+			arr[chotu]=tmp;
+			chotu--;
+		}
+	}
+}
+
+int main(){
+	int arr[]={3,1,4,2,6};
+	int s=sizeof(arr)/sizeof(arr[0]);
+	
+	for(int i=0; i<s; i++){
+		cout<<arr[i]<<"\n";
+	}
+	insrt(arr,s);
+	cout<<"\n After Applying Insertion Sort \n";
+	for(int i=0; i<s; i++){
+		cout<<arr[i]<<"\n";
+	}
+
+return 0;
+}
